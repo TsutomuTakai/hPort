cc41f7e3cb201304d3261dd81248a8b5ad6faa03
TsutomuTakai <wesley.takai@gmail.com>
2019-01-29 19:06:34 -0200

    menu mobile

--- src/js/main.js
+++ src/js/main.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
         // "ease-out", "ease-in-out", or even cubic bezier value such as "cubic-bezier(0.175, 0.885, 0.420, 1.310)"
         animationTime: 1300, // AnimationTime let you define how long each section takes to animate
         pagination: true, // You can either show or hide the pagination. Toggle true for show, false for hide.
-        updateURL: false, // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
+        updateURL: true, // Toggle this true if you want the URL to be updated automatically when the user scroll to each page.
         beforeMove: function (index) {
 
         }, // This option accepts a callback function. The function will be called before the page moves.
